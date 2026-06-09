51188ad4413f1bcbdd157eeb4d7eabdc31fd9c5e
jocke <git@joc.ke>
2026-06-09 14:37:44 +0200

    Add Terraform GitOps workflow and nginx container config

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/user/1000/podman/podman.sock"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.id
+  name  = "gitops-lab-nginx"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
