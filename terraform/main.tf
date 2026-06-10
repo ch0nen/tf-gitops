4e0b40bd36bb1f3ecf8dc008a9aef33e06e1e1f7
jocke <git@joc.ke>
2026-06-10 12:21:31 +0200

    Fix vulnerable image resource type and update to DVWA
    Replace invalid `vuln_image` resource type with `docker_image`
    and update the image from web-owasp to web-dvwa. Correct the
    resource references to use valid Terraform identifiers with
    hyphens instead of slashes.

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,8 @@ resource "docker_image" "nginx" {
   name = "nginx:${var.nginx_version}"
 }
 
-resource "vuln_image" "vulnerables/web-owasp" {
-  name = "vulnerables/web-owasp:latest"
+resource "docker_image" "vulnerables-web-dvwa" {
+  name = "vulnerables/web-dvwa:latest"
 }
 
 resource "docker_container" "nginx" {
@@ -28,9 +28,9 @@ resource "docker_container" "nginx" {
   }
 }
 
-resource "docker_container" "vulnerables/web-owasp" {
-  image = vuln_image.vulnerables.web-owasp.name
-  name  = "gitops-lab-vulnerables-web-owasp"
+resource "docker_container" "vulnerables-web-dvwa" {
+  image = docker_image.vulnerables-web-dvwa.name
+  name  = "gitops-lab-vulnerables-web-dvwa"
   ports {
     internal = 80
     external = var.external_port_2
